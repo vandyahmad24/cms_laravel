0ac2b2f976368f810363d59f4f78a182fcbba45c
vandyahmad24 <vandyahmad2404@gmail.com>
2020-05-24 21:43:00 +0700

    part 1

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('blog.index');
+});
+Route::get('/show', function () {
+    return view('blog.show');
 });
